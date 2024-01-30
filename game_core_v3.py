4ebe8883f9d4b31b582ec6d26b853cf7bb755d76
Monakov <ivan.monakov90@gmail.com>
2024-01-30 14:37:37 +0200

    game_core corecting try times

--- game_core_v3.py
+++ game_core_v3.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def score_game(random_predict) -> int:
-    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
@@ -11,7 +11,7 @@ def score_game(random_predict) -> int:
     """
     count_ls = []
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
